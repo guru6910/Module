2092e53de86234ad7dc0ed7d89c9fda511b27ea6
Gurudev Devkar <169146749+guru6910@users.noreply.github.com>
2024-06-20 17:03:12 +0530

    Delete module directory

--- module/dev.tf
+++
@@ -1,6 +0,0 @@
-module "ec2" {
-    source = "./ec2"
-    ami_id = "ami-0ca1f30768d0cf0e1"
-    instance_type = "t2.micro"
-   
-}
--- module/ec2/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "aarti" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  availability_zone = "us-west-1a"
-  
-}
